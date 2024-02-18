bffebb88386f5ae046e114da95cc75c65f9134c1
salladi <salladi@uncc.edu>
2024-02-18 13:06:18 -0500

    Added D3 JS

---
+++ src/AboutPage/AboutPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class AboutPage extends Component {
+  render() {
+    return (
+      <div>AboutPage</div>
+    )
+  }
+}
+
+export default AboutPage
